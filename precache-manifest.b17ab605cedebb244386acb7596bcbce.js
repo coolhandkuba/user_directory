d97d5ee845993b8d6d0b6f9487a560a23beedb11
Pendergast <coolhandkuba@hotmail.com>
2021-03-04 12:27:41 -0500

    Updates

---
+++ precache-manifest.b17ab605cedebb244386acb7596bcbce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1673ba2a4de4759d3dc3e12d6ecd755",
+    "url": "/user_directory/index.html"
+  },
+  {
+    "revision": "f0502039a5cf0d2ec136",
+    "url": "/user_directory/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "28351f01625575548aee",
+    "url": "/user_directory/static/css/main.0da05a2e.chunk.css"
+  },
+  {
+    "revision": "f0502039a5cf0d2ec136",
+    "url": "/user_directory/static/js/2.9ce86cb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user_directory/static/js/2.9ce86cb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28351f01625575548aee",
+    "url": "/user_directory/static/js/main.13254343.chunk.js"
+  },
+  {
+    "revision": "c1b0f80b937952ba0c6a",
+    "url": "/user_directory/static/js/runtime-main.8a700d73.js"
+  }
+]);
